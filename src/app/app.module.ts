d5c9a6e2b0599ac08cac70a4143bde9881236236
Prince <chattriprince@gmail.com>
2021-02-12 17:49:24 +0530

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatTableModule} from '@angular/material/table';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ChartsModule, ThemeService } from 'ng2-charts';
 import { AppComponent } from './app.component';
+import { StockTrackerComponent } from './stock-tracker/stock-tracker.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockTrackerComponent,
+    LineChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatCheckboxModule,
+    MatTableModule,
+    ChartsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NoopAnimationsModule
   ],
-  providers: [],
+  providers: [ThemeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
